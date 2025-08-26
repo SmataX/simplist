3d3223136786a7ec626fdb7ef6c7474f2edf6187
Mateusz Jaworski <mateuszjaworski@Mateuszs-MacBook-Air.local>
2025-08-26 19:23:30 +0200

    Fixed imports in models

--- src/common/models/task.py
+++ src/common/models/task.py
@@ -1,7 +1,8 @@
 from sqlmodel import SQLModel, Field, Relationship
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
-from .user import User
+if TYPE_CHECKING:
+    from .user import User
 
 class Task(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
--- src/common/models/user.py
+++ src/common/models/user.py
@@ -1,8 +1,9 @@
 from sqlmodel import SQLModel, Field, Relationship
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 from datetime import datetime
 
-from .task import Task
+if TYPE_CHECKING:
+    from .task import Task
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
